4783763d24ed1ccd40ea758d3d343ab2ee3d36a1
Thomas Granbohm <thomas.granbohm@gmail.com>
2020-10-12 22:22:07 +0200

    ⚡️ Now deletes messages!

--- bot.go
+++ bot.go
@@ -5,20 +5,27 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"regexp"
 	"strings"
 	"syscall"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
-	Token string
-	Prefix string
+	Token            string
+	Prefix           string
+	PrefixNeedsSpace bool
 )
 
 func init() {
 	flag.StringVar(&Token, "t", "", "Discord Bot Token")
-	flag.StringVar(&Prefix, "p", "!", "Prefix for messages")
+	flag.StringVar(&Prefix, "p", "gfm", "Prefix for messages")
 	flag.Parse()
+	if regexp.MustCompile(`^[a-zA-Z]+$`).MatchString(Prefix) {
+		Prefix += " "
+	}
 }
 
 func main() {
@@ -48,17 +55,42 @@ func main() {
 }
 
 func messageCreated(session *discordgo.Session, message *discordgo.MessageCreate) {
-	if message.Author.ID == session.State.User.ID {return}
-	if !strings.HasPrefix(message.Content, Prefix) {return}
+	if message.Author.ID == session.State.User.ID {
+		return
+	}
+	if !strings.HasPrefix(message.Content, Prefix) {
+		return
+	}
+	handleMessage(session, message.Message)
+}
 
+func handleMessage(session *discordgo.Session, message *discordgo.Message) {
 	s := strings.ToLower(string(message.Content[len(Prefix):]))
 
+	var returningMessage string
+
 	if s == "ping" {
-		session.ChannelMessageSend(message.ChannelID, "Pong!")
+		returningMessage = "Pong!"
 	} else if s == "pong" {
-		session.ChannelMessageSend(message.ChannelID, "Ping!")
+		returningMessage = "Ping!"
 	} else {
-		fmt.Println(s)
+		return
 	}
-	
+
+	sentMessage, err := session.ChannelMessageSend(message.ChannelID, returningMessage)
+	if err != nil {
+		return
+	}
+
+	go deleteMessage(3, session, message)
+	go deleteMessage(5, session, sentMessage)
+}
+
+func deleteMessage(seconds int, session *discordgo.Session, message *discordgo.Message) {
+	<-time.NewTimer(time.Duration(seconds) * time.Second).C
+
+	err := session.ChannelMessageDelete(message.ChannelID, message.ID)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
