420246096c7f74544852075ef9989f65e3bb7ee5
mathiasmagnusson <mathiasmagnussons@gmail.com>
2020-10-13 20:16:31 +0200

    Add and parse .env

--- main.go
+++ main.go
@@ -8,14 +8,34 @@ import (
 	"syscall"
 
 	"github.com/Bumbodosan/Gummi-Flying-Machine/bot"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	b := &bot.Bot{}
 
+	godotenv.Load();
+
 	flag.StringVar(&b.Token, "token", "", "Discord Bot Token")
 	flag.StringVar(&b.Prefix, "prefix", "", "Command prefix")
 	flag.Parse()
+	if b.Token == "" {
+		var found bool
+		b.Token, found = os.LookupEnv("TOKEN")
+		if !found {
+			fmt.Println("Missing token")
+			os.Exit(-1)
+		}
+	}
+	if b.Prefix == "" {
+		var found bool
+		b.Prefix, found = os.LookupEnv("PREFIX")
+		if !found {
+			fmt.Println("Missing prefix")
+			os.Exit(-1)
+		}
+	}
 
 	if err := b.Start(); err != nil {
 		fmt.Println(err)
